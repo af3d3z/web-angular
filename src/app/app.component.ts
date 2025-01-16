bb92e5301dc7a9ec7c07f26d6f73cd769fc1689e
af3d3z <alonso.fernandez@iesnervion.es>
2025-01-16 14:35:33 +0100

    aaaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NgOptimizedImage} from '@angular/common';
+import {TablaPersonasComponent} from './components/tabla-personas/tabla-personas.component';
+import {FormularioPersonaComponent} from './components/formulario-persona/formulario-persona.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgOptimizedImage],
+  imports: [RouterOutlet, NgOptimizedImage, TablaPersonasComponent, FormularioPersonaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
