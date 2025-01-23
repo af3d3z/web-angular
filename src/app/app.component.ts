0c139ccddf99d30daee434869fc69de3d7e6a4c8
af3d3z <alonso.fernandez@iesnervion.es>
2025-01-23 14:36:02 +0100

    salgo a la calle con los rulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {NgOptimizedImage} from '@angular/common';
 import {TablaPersonasComponent} from './components/tabla-personas/tabla-personas.component';
 import {FormularioPersonaComponent} from './components/formulario-persona/formulario-persona.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgOptimizedImage, TablaPersonasComponent, FormularioPersonaComponent],
+  imports: [RouterOutlet, NgOptimizedImage, TablaPersonasComponent, FormularioPersonaComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import {TablaPersonasComponent} from './components/tabla-personas/tabla-personas.component';
+import {FormularioPersonaComponent} from './components/formulario-persona/formulario-persona.component';
 import { Routes } from '@angular/router';
+import {ListadoComponent} from './listado/listado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'tabla', component: TablaPersonasComponent},
+  {path: 'formulario', component: FormularioPersonaComponent},
+  {path: 'listado', component: ListadoComponent}
+];
