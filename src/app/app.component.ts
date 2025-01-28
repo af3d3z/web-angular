fc4c345476cadee14a28e67e72d5afd7afb54813
af3d3z <alonso.fernandez@iesnervion.es>
2025-01-28 14:38:15 +0100

    ayuda bombardeen angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {NgOptimizedImage} from '@angular/common';
 import {TablaPersonasComponent} from './components/tabla-personas/tabla-personas.component';
 import {FormularioPersonaComponent} from './components/formulario-persona/formulario-persona.component';
+import {MatSlideToggle} from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgOptimizedImage, TablaPersonasComponent, FormularioPersonaComponent, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, NgOptimizedImage, TablaPersonasComponent, FormularioPersonaComponent, RouterLink, RouterLinkActive, MatSlideToggle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
